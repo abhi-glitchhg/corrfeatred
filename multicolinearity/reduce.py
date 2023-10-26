90772872452ccd56f848f89586f4ac4ea039bfce
abhi_win <f20190041@goa.bits-pilani.ac.in>
2023-10-26 07:49:30 +0530

    optimisation and minor bug fix

--- multicolinearity/reduce.py
+++ multicolinearity/reduce.py
@@ -2,64 +2,68 @@ import numpy as np
 from collections import defaultdict
 import networkx as nx
 from networkx.algorithms import clique
+import pandas as pd
+
+def reduce_features(corr_matrix, threshhold=0.0,):
 
 
-def reduce_features(corr_matrix, threshhold=0.0):
-
-
-    """
-    correlation matrix : df.corr
-    threshhold: float
-    method: 
-    """
-
-    # if summ >=2 then self loop zero 
+#     """
+#     correlation matrix : df.corr
+#     threshhold: float
+#     method:
+#     """
+    feature_set = []
+    del_this = []
+    cols = np.array(corr_matrix.columns)
+    corr_matrix = corr_matrix.values.astype(bool)
+    # if summ >=2 then self loop zero
     for i in range(corr_matrix.shape[0]):
         if corr_matrix[i].sum() >=2:
             corr_matrix[i][i]=0
+        else:
+            feature_set.append(cols[i])
+            del_this.append(i)
+    corr_matrix = np.delete(np.delete(corr_matrix, del_this, axis=0), del_this, axis=1)
+    cols_new = np.delete(cols, del_this, axis=0)
+    G = nx.Graph(corr_matrix)    
     
-
-    G = nx.Graph(corr_matrix)     
-     
     cliques = list(clique.find_cliques(G))
+    # print(cliques)
     cliques = sorted(cliques, key=lambda x: len(x), reverse=True)
     mask = np.zeros(len(cliques), dtype=bool)
-    feature_set = []
-     
+   
     dict1 = defaultdict(lambda : 0)
     dict2 = defaultdict(list)
-    
+    # print("lfg")
     for idx, clq in enumerate(cliques):
             for key in clq:
-                dict1[key]+=1
-                dict2[key].append(idx)
-    while not np.all(mask): 
+                dict1[key]+=1 # a node present in how many groups
+                dict2[key].append(idx) # node to group mapping
+    while not np.all(mask):
 
-        # print(dict1, "\n", dict2)
-
-        
         sorted_list = sorted(dict1.items(), key = lambda kv: kv[1], reverse=True)
         # print(sorted_list)
-        
+       
         top = sorted_list[0]
-        # print(top)
-        feature_set.append(top[0])
+        if top[1] <=0 : break
+        feature_set.append(cols_new[top[0]])
+       
+        assert sum(mask[dict2[top[0]]]) == 0, "sum should be zero here"
+       
         mask[dict2[top[0]]] = True
-        # print(mask)
-        
+       
         for i in dict2[top[0]]:
             for j in cliques[i]:
                 dict1[j]-= 1
-
-        # print("iter finished \n\n\n")
+        # direct connections with top[0] must be made 0;
+       
+        for i in np.where(corr_matrix[top[0]])[0]: dict1[i] = 0
+        dict1[top[0]] = 0
+       
     return feature_set
 
-
-
-
-
 if __name__ == "__main__":
-    adjacency_matrix = np.array([
+    adjacency_matrix = pd.DataFrame(np.array([
     
                              [1,1,1,0,0,0,1,1,0,0], #a
                              [1,1,1,0,0,0,0,0,0,0], #b
@@ -71,7 +75,7 @@ if __name__ == "__main__":
                              [1,0,0,0,0,1,1,1,1,0], #h
                              [0,0,0,0,0,1,1,1,1,0], #i
                              [0,0,0,0,0,0,0,0,0,1]
-                             ])
+                             ]))
     
     a = reduce_features(adjacency_matrix,0.0)
 
