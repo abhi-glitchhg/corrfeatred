3ed131f508b577673f19b58382970f44737b01ff
abhi_win <f20190041@goa.bits-pilani.ac.in>
2023-10-29 20:06:17 +0530

    cleanup

--- corrfeatred/reduce.py
+++ corrfeatred/reduce.py
@@ -3,27 +3,27 @@ import numpy as np
 from collections import defaultdict
 import networkx as nx
 from networkx.algorithms import clique
-import pandas as pd
 import random
 
-def reduce_features(corrmatrix_, threshhold=0.75,method_='min', random_state=None):
+def reduce_features(correlation_matrix, threshhold=0.75,policy='min', random_state=None):
 
 
 #     """
-#     correlation matrix : df.corr
+#     correlation_matrix : df.corr
 #     threshhold: float
-#     method: `min` or `max` default is min 
+#     method: whether we want minimum number of features or maximum number of features. NOTE: this is bit unstable and sometimes `min` policy has more features than `max`, this depends on how graph develops. 
+#     random_state: random state, use this to get different set of features for same correlation matrix.  
 #     """
 
     if random_state!=None:
         random_gen = random.Random(random_state)
-    corrmatrix = corrmatrix_.copy()
-    inf_ = 4*(corrmatrix.shape[0]) + 10 # adding 10 for no reason, this could be any positive number; but messi is GOAT.
+    corrmatrix = correlation_matrix.copy()
+    inf_ = 4*(corrmatrix.shape[0]) + 10 # adding 10 for no reason, this could be any positive number;
     corr_matrix = corrmatrix > threshhold
-    assert method_ in ('min', 'max'), "wrong input parameter"
-    method = max if method_ == 'min' else min
+    assert policy in ('min', 'max'), "wrong input parameter"
+    method = max if policy == 'min' else min
     feature_set = []
-    del_this = []
+    del_this = [] # to track features which are not correlated with any other feature. this will reduce the graph calculations
     cols = np.array(corr_matrix.columns)
     corr_matrix = corr_matrix.values.astype(bool)
     # if summ >=2 then self loop zero
@@ -37,24 +37,23 @@ def reduce_features(corrmatrix_, threshhold=0.75,method_='min', random_state=Non
     cols_new = np.delete(cols, del_this, axis=0)
     G = nx.Graph(corr_matrix)    
     cliques = list(clique.find_cliques(G))
-    #print(cliques)
-    #nx.draw(G)
+    
     cliques = sorted(cliques, key=lambda x: len(x), reverse=True)
     mask = np.zeros(len(cliques), dtype=bool)
    
-    dict1 = defaultdict(lambda : 0)
-    dict2 = defaultdict(list)
-    # print("lfg")
+    node_to_clique_count = defaultdict(lambda : 0) # this dictionary keeps updated as we select features.
+    node_to_clique_list = defaultdict(list) # this dictionary is not updated  
+
     for idx, clq in enumerate(cliques):
             for key in clq:
-                dict1[key]+=1 # a node present in how many groups
-                dict2[key].append(idx) # node to group mapping
+                node_to_clique_count[key]+=1 # a node present in how many groups
+                node_to_clique_list[key].append(idx) # node to group mapping
     while not np.all(mask):
 
         if random_state==None:
-            top = method(dict1.items(), key = lambda kv: kv[1])
+            top = method(node_to_clique_count.items(), key = lambda kv: kv[1])
         else:
-            dict_items = list(dict1.items())
+            dict_items = list(node_to_clique_count.items())
             random_gen.shuffle(dict_items)
             top = method(dict_items, key = lambda kv: kv[1])
             del dict_items
@@ -64,23 +63,19 @@ def reduce_features(corrmatrix_, threshhold=0.75,method_='min', random_state=Non
         feature_set.append(cols_new[top[0]])
        
        
-        mask[dict2[top[0]]] = True
-       
-        for i in dict2[top[0]]:
-            for j in cliques[i]:
-                dict1[j]-= 1
+        mask[node_to_clique_list[top[0]]] = True
         
         for i in np.where(corr_matrix[top[0]])[0]: 
-          if method_ == 'min': 
-            dict1[i] = 0
+          if policy == 'min': 
+            node_to_clique_count[i] = 0
           else:
-            dict1[i] = inf_ 
+            node_to_clique_count[i] = inf_ 
 
-        if method_ == 'min': 
-          dict1[top[0]] = 0
+        if policy == 'min': 
+          node_to_clique_count[top[0]] = 0
         else:
-          dict1[top[0]] = inf_
-        #print(dict1.items())
+          node_to_clique_count[top[0]] = inf_
+        
        
     del corrmatrix
     return feature_set
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -7,7 +7,7 @@ def test_hello():
 
 
 def test_correlation():
-    for i in (4,8,10,12,16,22,100,1000,5000,10000,15000, 20000):
+    for i in (4,8,10,12,16,22,100,1000,5000):
         rand_array = np.random.uniform(0,1,i*i).reshape(i,i)
         corr_arr = np.tril(rand_array) + np.triu(rand_array.T,1)
         assert (corr_arr!=corr_arr.T).sum().sum() == 0
@@ -15,10 +15,13 @@ def test_correlation():
             corr_arr[j][j] = 1
         
         corr_matrix = pd.DataFrame(corr_arr,columns=[str(k) for k in range(i)], index=[str(k) for k in range(i)])
-        for threshhold in (0.3,0.4,0.6,0.8,.99):
+        for threshhold in (0.6,0.8,.99):
             print(i,threshhold)
-            feats_min = reduce_features(corr_matrix, threshhold=threshhold, method_= 'min')
-            feats_max = reduce_features(corr_matrix, threshhold=threshhold, method_='max')
+            feats_min = reduce_features(corr_matrix, threshhold=threshhold, policy= 'min')
+            feats_max = reduce_features(corr_matrix, threshhold=threshhold, policy='max')
 
-            assert (corr_matrix[feats_max].loc[feats_max]>threshhold).sum().sum() == len(feats_max), f"{(corr_matrix[feats_max].loc[feats_max]>threshhold).sum().sum()} {len(feats_max)}" 
+            assert (corr_matrix[feats_max].loc[feats_max]>threshhold).sum().sum() == len(feats_max)
             assert (corr_matrix[feats_min].loc[feats_min]>threshhold).sum().sum() == len(feats_min)
+
+
+
